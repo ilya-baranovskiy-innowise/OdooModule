e7b647b355bdee143bf4f76f8b4b72170a01b96b
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2025-09-05 16:43:29 +0300

    [NEW] first module: Module-1 add folder structure, model and form view

---
+++ first_module/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "First Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Ilya",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    'data': [
+        'views/first_view.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': True,
+}
