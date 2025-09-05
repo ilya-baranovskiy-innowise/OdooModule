46e362886d385fd0832f18d5daa50a5fe8b57170
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2025-09-06 00:57:26 +0300

    [IMP] first modul: MODULE-1 add graph view and learn decorators: depends, onchange, constrains and method compute. Add comments to functions

--- first_module/models/first_model.py
+++ first_module/models/first_model.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class FirstModel(models.Model):
@@ -33,5 +34,25 @@ class FirstModel(models.Model):
 
     @api.depends('check_all')
     def _compute_check_all_int(self):
+        """compute check_all row from bool to int and write result in new the row"""
         for rec in self:
             rec.check_all_int = 1 if rec.check_all else 0
+
+    @api.constrains('text')
+    def _check_text_len(self):
+        """check text length"""
+        for record in self:
+            if not record.text or len(record.text) < 5:
+                raise ValidationError("Short text")
+
+    @api.onchange('text')
+    def _check_text_len_before_save(self):
+        """check text length before save"""
+        for record in self:
+            if not record.text or len(record.text) < 5:
+                return {
+                    'warning': {
+                        'title': "Error",
+                        'message': "Short text len",
+                    }
+                }
