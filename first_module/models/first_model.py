b2082504401de8174133f813f6a770f641910cb4
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2025-09-06 20:28:56 +0300

    [IMP] first_module: TASK 3 work with check_boxes

--- first_module/models/first_model.py
+++ first_module/models/first_model.py
@@ -38,8 +38,8 @@ class FirstModel(models.Model):
     ])
 
     # digit fields
-    check_all_int = fields.Integer(string='Check All as Integer', compute='_compute_check_all_int',
-                                   inverse='_inverse_check_boxes', store=True)
+    # check_all_int = fields.Integer(string='Check All as Integer', compute='_compute_check_all_int',
+    # inverse='_inverse_check_boxes', store=True)
     amount = fields.Float(string='Amount')
     price = fields.Monetary(string='Price', currency_field='currency_id')
 
@@ -63,20 +63,38 @@ class FirstModel(models.Model):
     json_data = fields.Json(string='Json data')
     menu_visible = fields.Boolean(default=True, compute='_compute_menu_visible', store=True)
 
+    @api.onchange('check_all')
+    def _onchange_check_all(self):
+        if self.check_all:
+            self.check1 = True
+            self.check2 = True
+        else:
+            self.check1 = False
+            self.check2 = False
+
+    @api.onchange('check1', 'check2')
+    def _onchange_check1_check2(self):
+        if self.check1 and self.check2:
+            self.check_all = True
+        else:
+            if self.check_all:
+                self.check_all = False
+
     @api.depends('target_datetime')
     def _compute_menu_visible(self):
+        """compute menu visible"""
         for rec in self:
             if rec.menu_visible:
                 rec.menu_visible = False
 
-    @api.depends('check_all')
+    """@api.depends('check_all')
     def _compute_check_all_int(self):
-        """compute check_all row from bool to int and write result in new the row"""
+        ""compute check_all row from bool to int and write result in new the row""
         for rec in self:
             rec.check_all_int = 1 if rec.check_all else 0
 
     def _inverse_check_boxes(self):
-        """inverse method to change check1 check2 inputs"""
+        ""inverse method to change check1 check2 inputs""
         for rec in self:
             flag = bool(rec.check_all_int)
             rec.check1 = not flag
@@ -84,14 +102,14 @@ class FirstModel(models.Model):
 
     @api.constrains('text')
     def _check_text_len(self):
-        """check text length"""
+        ""check text length""
         for record in self:
             if not record.text or len(record.text) < 5:
                 raise ValidationError("Short text")
 
     @api.onchange('text')
     def _check_text_len_before_save(self):
-        """check text length before save"""
+        ""check text length before save""
         for record in self:
             if not record.text or len(record.text) < 5:
                 return {
@@ -99,4 +117,4 @@ class FirstModel(models.Model):
                         'title': "Error",
                         'message': "Short text len",
                     }
-                }
+                }"""
